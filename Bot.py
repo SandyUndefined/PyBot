42bcc5a6f18634fb2de29a6fc57776109be2ad2a
Sandy_undefined <08sandysk@gmail.com>
2020-04-29 07:13:45 +0530

    saving

--- Bot.py
+++ Bot.py
@@ -1,13 +1,28 @@
-# from selenium import webdriver
-# PATH = "C:\Program Files (x86)\chromedriver.exe"
-#
-# driver = webdriver.Chrome(PATH)
-# driver.get("https://google.com")
-# print(driver.title)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+driver.get("https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp")
+print(driver.title)
+
+firstName = driver.find_element_by_name("firstName")
+firstName.send_keys("Ramesh")
+
+lastName = driver.find_element_by_name("lastName")
+lastName.send_keys("Jaiswal")
+
+email = driver.find_element_by_name("Username")
+email.send_keys("RameshJ09876")
+
+password = driver.find_element_by_name("Passwd")
+password.send_keys("@Sandeep0987654")
+
+confirm = driver.find_element_by_name("ConfirmPasswd")
+confirm.send_keys("@Sandeep0987654")
+
+firstName.send_keys(Keys.RETURN)
+# time.sleep(5)
 # driver.quit()
 
-import pyperclip
-pyperclip.copy("You'r account has been hacked")
-pyperclip.paste
-
-print(None)
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-import requests,bs4
+# import pyperclip
+# pyperclip.copy("You'r account has been hacked")
+# pyperclip.paste
+# print(None)
+
+import bs4
+import requests
 
 res = requests.get('https://www.google.com')
 res.raise_for_status()
+text = bs4.BeautifulSoup(res.text,'html.parser')
+print(type(text))
